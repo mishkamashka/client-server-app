d604cca1018f7b377863ce8fb8f919614f139642
Mariia Anisimova <just-another-log-in@yandex.ru>
2018-11-12 19:39:02 +0300

    changed structure

--- socket-server/file_io.h
+++
@@ -1,9 +0,0 @@
-#ifndef FILE_IO_H
-#define FILE_IO_H
-
-bool save(linked_list_t *linked_list, const char *filename);
-bool load(linked_list_t **p_linked_list, const char* filename);
-bool serialize(linked_list_t* linked_list, const char* filename);
-bool deserialize(linked_list_t** p_linked_list, const char* filename);
-
-#endif
